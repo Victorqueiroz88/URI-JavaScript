d52f69e2a0359fedfa7233d31fdd5d87fee36445
Victor Queiroz <victorcohen1799@gmail.com>
2021-10-20 18:24:23 -0300

    Meu primeiro commit

---
+++ Iniciante/1000/1000.js
@@ -0,0 +1,10 @@
+let saudacao = 'olá mundo';
+let saudacao2 = 'ok';
+
+saudacao = 2000;
+
+console.log(saudacao);
+console.log(saudacao2);
+
+console.log(typeof saudacao, 'tipo da variável saudação')
+console.log(typeof saudacao2, 'tipo da variável saudação2')
